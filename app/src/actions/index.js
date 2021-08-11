b25bb12f78b24f00c13279112b237e717c43c79c
Rick <rmansfield@realpeoplesratings.com>
2021-08-11 16:00:27 -0500

    Set up Actions folder with creators

---
+++ app/src/actions/index.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+export const getDog = () => {
+    return (dispatch) => {
+        dispatch(fetchStart());
+        axios.get('https://dog.ceo/api/breeds/image/random')
+        .then(res=>{
+            dispatch(fetchSuccess(res.data.message))
+        })
+        .catch(err => {
+            dispatch(fetchFail(err));
+        });
+    }
+}
+
+export const FETCH_START = "FETCH-START";
+export const fetchStart = () => {
+    return({type: FETCH_START});
+}
+
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+export const fetchSuccess = (dog)=> {
+    return({type: FETCH_SUCCESS, payload: dog});
+}
+
+export const FETCH_FAIL = "FETCH_FAIL";
+export const fetchFail = (error)=> {
+    return({type: FETCH_FAIL, payload: error});
+}
